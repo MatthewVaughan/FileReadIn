75a9a6211bec38e0ec137d2ff3c8a2466621076e
MatthewVaughan <mattman94@gmail.com>
2016-06-22 21:23:36 -0600

    original upload

---
+++ Proj11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+// File/Project Prolog
+// Name: Matthew Vaughan
+// CS 1400 Section 003
+// Project: Project 11
+// Date: 04/15/2016 09:15:19 pm
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+namespace Proj11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Display instructions
+            WriteLine("Resistor Batch Test Analysis Program");
+            WriteLine("Data file must be in your documents folder");
+            Write("Please enter the file name: ");
+
+            //Declare file name and take users input to find appropriate folder
+            string fileName = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + @"\";
+            fileName += ReadLine();
+
+            StreamReader theTextFile = new StreamReader(fileName);
+
+            Resistance rs = new Resistance(theTextFile.ReadLine());
+
+            while (theTextFile.EndOfStream == false)
+            {
+                rs.AddDate(theTextFile.ReadLine());
+            }
+
+
+            WriteLine("\n\n");
+            WriteLine("Res#\tDissipation\tPassed");
+            for (int i = 0; i < rs.GetLength(); i++)
+            {
+                WriteLine($"{i + 1}\t{rs.GetPowerDissipated(i):f2}\t\t{rs.GetTestPassFail(i)}");
+            }
+            
+
+            ReadLine();
+        }
+    }
+}
---
+++ Proj11/Resistance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj11
+{
+    class Resistance
+    {
+        private double specifiedResistance;
+        private double specifiedPower;
+        private double[] voltages;
+        private int index;
+
+        public Resistance(string data)
+        {
+            //Save the data input and split the data received
+            string[] inputs = data.Split();
+
+            specifiedResistance = double.Parse(inputs[0]);
+            specifiedPower = double.Parse(inputs[1]);
+
+            voltages = new double[50];
+            index = 0;
+        }
+
+        public void AddDate(string input)
+        {
+            voltages[index++] = double.Parse(input);
+        }
+
+        public int GetLength()
+        {
+            return index;
+        }
+
+        public double GetPowerDissipated(int i)
+        {
+            return voltages[i] * voltages[i] / specifiedResistance;
+        }
+
+        public string GetTestPassFail(int i)
+        {
+            if (GetPowerDissipated(i) > specifiedPower)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+    }
+}
